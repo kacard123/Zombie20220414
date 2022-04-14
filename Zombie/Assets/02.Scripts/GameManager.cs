83e31c6bedb352d2c9bbc1a2820b7089082da978
LimJuYeong <shalvah623@naver.com>
2022-04-14 16:19:05 +0900

    20220414

---
+++ Zombie/Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public bool isGameover;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
